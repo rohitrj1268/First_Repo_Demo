91b376054fd50ac4f7f44325021040ccb5c4c41d
rohitrj1268 <rohitjadhav1268@gmail.com>
2025-10-10 14:44:49 +0530

    Revert "This is new feature for branch"

--- src/com/package1/Person.java
+++ src/com/package1/Person.java
@@ -6,7 +6,6 @@ public class Person {
 		// TODO Auto-generated method stub
        System.out.println("hello");
        System.out.println("B commit msg");
-       System.out.println("switched to new feature");
 	}
 
 }
