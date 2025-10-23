10c8416689172d941d146da9a473f31e86248eff
Alfodrr <jaydethe19@gmail.com>
2025-10-24 01:01:24 +0530

    Deleted MINI PROJECT from repo

--- MINI_PROJECT_1/src/com/filemanagement/FilePath.java
+++
@@ -1,7 +0,0 @@
-package com.filemanagement;
-
-public interface FilePath {
-String FILE_PATH = "d:\\JAVA FULL STACK\\MINI_PROJECT_1\\ID.txt";
-String MASTER_PATH = "masterfile.txt";
-String TRANS_PATH = "trans.txt";
-}
